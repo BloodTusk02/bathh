38def23aa21122eb2da676a4ef61003caf6d9174
BloodTusk <romarozhnov02@gmail.com>
2022-02-05 02:33:23 +0700

    added mobile version

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ const options = {
 //Инициализируем окно. Созданный объект заносим в "scrollbar". 
 //Дальнейшее взаимодействие производить надо через этот объект
 const scrollbar = Scrollbar.init(document.querySelector('#my-scrollbar'), options);
-
 const navBar = document.querySelector(".navbar");
 const allLi = document.querySelectorAll("li");
+const menuItems = document.querySelector("#fixed > ul:nth-child(1)").children;
 
 allLi.forEach((li, index) => {
     li.addEventListener("click" , e => {
@@ -20,40 +20,60 @@ allLi.forEach((li, index) => {
     });
 });
 
-
-let baseOffset = scrollbar.offset.y;
-let prevActiveIndex = Math.floor(scrollbar.offset.y  / window.screen.width);
-const menuItems = document.querySelector("#fixed > ul:nth-child(1)").children;
-
 //Events
 document.addEventListener("pageChanged", (e) => {
     menuItems[e.detail.prevPage].classList.remove('active');
     menuItems[e.detail.currPage].classList.add('active');
 });
 
+if (document.documentElement.clientWidth > 600) {
+    var baseOffsetLeft = fixed.offsetLeft;
+    var baseOffsetTop = fixed.offsetTop;
+    let prevActiveIndex = Math.floor(scrollbar.offset.y  / window.screen.width + 0.5);
 
-//Вроде как это будет срабатывать при любом изменении состояния скроллбара
-scrollbar.addListener(function(status) {
-    //console.log(status.offset); //Вывод координат
-    let offset = status.offset;
+    //Вроде как это будет срабатывать при любом изменении состояния скроллбара
+    scrollbar.addListener(function(status) {
+        let offset = status.offset;
 
-    fixed.style.top = offset.x + 'px';
-    fixed.style.left = offset.y + 'px';
+        fixed.style.top = (offset.x + baseOffsetTop).toString() + 'px';
+        fixed.style.left = (offset.y + baseOffsetLeft).toString() + 'px';
 
-    //Смена страниц
-    //const prevItem = document.querySelector(".active");
-    currActiveIndex =  Math.floor(offset.y  / window.screen.width);
-    if(prevActiveIndex != currActiveIndex){
-        const eventDetalis = { 
-            detail:{
-                prevPage: prevActiveIndex,
-                currPage: currActiveIndex
-            }
-        };
-        prevActiveIndex = currActiveIndex;
+        //Смена страниц
+        currActiveIndex =  Math.floor(offset.y  / window.screen.width + 0.5);
+        if(prevActiveIndex != currActiveIndex) {
+            const eventDetalis = { 
+                detail:{
+                    prevPage: prevActiveIndex,
+                    currPage: currActiveIndex
+                }
+            };
+            prevActiveIndex = currActiveIndex;
 
-        const pageChangedEvent = new CustomEvent("pageChanged", eventDetalis);
-        
-        document.dispatchEvent(pageChangedEvent)
-    }
-});
+            const pageChangedEvent = new CustomEvent("pageChanged", eventDetalis);
+            
+            document.dispatchEvent(pageChangedEvent);
+        }
+    });
+} else {
+    let prevActiveIndex = Math.floor(scrollbar.offset.y  / window.screen.height + 0.5);
+
+    scrollbar.addListener(function(status) {
+        let offset = status.offset;
+
+        //Смена страниц
+        currActiveIndex =  Math.floor(offset.y  / window.screen.height + 0.5);
+        if(prevActiveIndex != currActiveIndex){
+            const eventDetalis = { 
+                detail:{
+                    prevPage: prevActiveIndex,
+                    currPage: currActiveIndex
+                }
+            };
+            prevActiveIndex = currActiveIndex;
+
+            const pageChangedEvent = new CustomEvent("pageChanged", eventDetalis);
+            
+            document.dispatchEvent(pageChangedEvent);
+        }
+    });
+}
